fef57fc6bbbc17638687f4b53700ae205262ab5c
Gustavo H. Torres <gus7ht@gmail.com>
2026-03-19 17:23:30 +0100

    defined class 'circle'

---
+++ circle.py
@@ -0,0 +1,9 @@
+class circle:
+    def __init__(self, radius):
+        self.radius = radius
+    
+    def area(radius):
+        return np.pi * (self.radius ** 2)
+    
+    def perimeter(self):
+        return np.pi * 2 * self.radius
